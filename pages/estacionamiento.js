004b19a814662e86ae3e79938560ea2add430d0a
Gustavo <gustavoaguilarmartinez891@gmail.com>
2025-11-10 17:22:17 -0600

    integracion de plantilla principal dashboard

--- pages/estacionamiento.js
+++ pages/estacionamiento.js
@@ -1,11 +1,10 @@
 // pages/estacionamiento.js
 
-export default function Estacionamiento() {
+export default function Estacionamiento() { 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-800">
-        Hello Parking 🚗
-      </h1>
+    <div className="flex items-center justify-center min-h-screen bg-gray-300">
+        <img alt="Imagen de carro animado" src="/images/logobacground.png" className="z-[0]"></img>
+    
     </div>
   );
 }
