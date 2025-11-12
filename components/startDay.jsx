e3e69bc4af28f80a5b703a68d848cefe309de2c6
Gustavo <gustavoaguilarmartinez891@gmail.com>
2025-11-11 21:27:18 -0600

    integracion de componente para startday

---
+++ components/startDay.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function StartDay() {
+
+    return(
+            <button className="bg-red-700 text-white p-3 rounded-2xl absolute">Comenzar Dia</button>
+    );
+}
