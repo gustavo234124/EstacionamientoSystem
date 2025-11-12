95262bb1e8eb98ad7557b63eb92f41fa7bac1822
Gustavo <gustavoaguilarmartinez891@gmail.com>
2025-11-11 22:09:14 -0600

    actualizacion de posicionamiento en boton activo y ocultos

--- pages/estacionamiento.js
+++ pages/estacionamiento.js
@@ -9,11 +9,7 @@ export default function Estacionamiento() {
       
       {/* Contenido con padding izquierdo en PC para que no quede debajo del navbar */}
       <div className="md:ml-64 flex items-center justify-center min-h-screen p-4">
-        <img 
-          alt="Imagen de carro animado" 
-          src="/images/logobacground.png" 
-        />
-
+        
         <StartDay />
 
 
